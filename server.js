2c2441bc57e1dc8732a966d84c1f4a149ee1fe36
Jake Krayger <jkrayg@protonmail.com>
2020-05-16 17:48:52 -0400

    heroku not working

--- server.js
+++ server.js
@@ -6,16 +6,14 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(methodOverride('_method'));
 
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({
-    defaultLayout: "main"
-}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burger_controller.js");
