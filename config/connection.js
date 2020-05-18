44e618b9334ad538e3674f8b29aee63e6ae638fe
Jake Krayger <jkrayg@protonmail.com>
2020-05-18 14:50:18 -0400

    srg

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,30 @@
-var mysql = require("mysql");
+var mysql = require('mysql');
+
+// Create the MySQL connection object
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  // DB is JawsDB on Heroku
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Rootpassword3574",
-    database: "burgers_db"
-  });
-}
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'Rootpassword3574',
+		database: 'burgers_db'
+	})
+};
 
-
-
-connection.connect(function (err) {
+// Make the connection to MySQL
+connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
 });
 
+// Export connection for ORM use
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,29 +1,28 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
+var express = require('express');
 var bodyParser = require('body-parser');
-var methodOverride = require("method-override")
+var methodOverride = require('method-override');
+
+var port = process.env.PORT || 3000;
 
 var app = express();
-var PORT = process.env.PORT || 3000;
 
-
+// Serve static content for the app from the 'public' directory
 app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.json());
 
+// Override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 
+// Set Handlebars as the view engine
+var exphbs = require('express-handlebars');
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
+// Import routes and give the server access to them
+var routes = require('./controllers/burger_controller.js');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.use('/', routes);
 
-var routes = require("./controllers/burger_controller.js");
-
-app.use(routes);
-
-app.listen(PORT, function () {
-    console.log("Server listening on: http://localhost:" + PORT);
-});
+app.listen(port);
